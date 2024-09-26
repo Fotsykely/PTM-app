fbfd3d5ff7b3345bcfd0b0b1816843bb8487e57a
Yuuta69 <a.potetoky@gmail.com>
2024-09-26 20:15:36 +0300

    modif

--- app/Views/dashboard/edit_task.php
+++ app/Views/dashboard/edit_task.php
@@ -13,7 +13,7 @@
         <form action="<?= site_url('Project/updateTask/'.$project['id']) ?>" method="post">
         <div class="form-group">
                 <label for="project_name">Project Name:</label>
-                <input type="text" name="project_name" id="task_name" value="<?= $project['project_name'] ?>" required>
+                <input type="text" name="project_name" id="project_name" value="<?= $project['project_name'] ?>" required>
             </div>
 
             <div class="form-group">
