1572fe80a90bc025566695171c8866cf7332ab14
Yuuta69 <a.potetoky@gmail.com>
2024-10-18 11:54:28 +0300

    MAJ : ajout design login&signin

--- app/Views/entry/login.php
+++ app/Views/entry/login.php
@@ -1,37 +1,42 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login Page</title>
+    <link rel="stylesheet" href="/css/entry.css">
 </head>
 
 <body>
     <div class="container">
         <div class="card">
             <div class="card-header">
-                <h2>Login</h2>
+                <h2>Login Here</h2>
             </div>
             <div class="card-body">
                 <?php if(session()->getFlashdata('msg')):?>
-                <div class="alert alert-warning text-center">
+                <div class="alert">
                    <?= session()->getFlashdata('msg') ?>
                 </div>
                 <?php endif;?>
                 <form action="<?php echo base_url(); ?>/LoginController/login" method="post">
-                    <div class="form-group mb-3">
-                        <input type="email" name="email" placeholder="Email" class="form-control">
+                    <div class="form-group">
+                        <input type="email" name="email" placeholder="Email or Phone" class="form-control">
                     </div>
-                    <div class="form-group mb-3">
+                    <div class="form-group">
                         <input type="password" name="password" placeholder="Password" class="form-control">
                     </div>
-                    <div class="d-grid">
-                        <button type="submit" class="btn btn-dark">Submit</button>
+                    
+                    <button type="submit" class="btn">Log In</button>
+                    
+                    <div class="form-other">
+                        <a href="<?= site_url('/signin') ?>" class="register-link">Don't have an account? Sign up here</a>
                     </div>
-                    <a href="<?= site_url('/signin') ?>" class="d-block text-center mt-3">Don't have an account? Sign up here</a>
                 </form>
             </div>
         </div>
     </div>
 </body>
+
 </html>
--- app/Views/entry/signin.php
+++ app/Views/entry/signin.php
@@ -1,42 +1,49 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register Page</title>
+    <link rel="stylesheet" href="/css/entry.css">
 </head>
 
 <body>
     <div class="container">
         <div class="card">
             <div class="card-header">
-                <h2>Register</h2>
+                <h2>Register Here</h2>
             </div>
             <div class="card-body">
-                <?php if(isset($validation)):?>
-                <div class="alert alert-warning text-center">
+                <?php if(isset($validation)): ?>
+                <div class="alert">
                    <?= $validation->listErrors() ?>
                 </div>
-                <?php endif;?>
-                <form action="<?php echo base_url(); ?>/SigninController/store" method="post">
-                    <div class="form-group mb-3">
+                <?php endif; ?>
+                <form action="<?php echo base_url(); ?>/SigninController/store" method="post" class="form-signin">
+                    <div class="form-group">
                         <input type="text" name="username" placeholder="Username" value="<?= set_value('username') ?>" class="form-control">
                     </div>
-                    <div class="form-group mb-3">
+                    <div class="form-group">
                         <input type="email" name="email" placeholder="Email" value="<?= set_value('email') ?>" class="form-control">
                     </div>
-                    <div class="form-group mb-3">
+                    <div class="form-group">
                         <input type="password" name="password" placeholder="Password" class="form-control">
                     </div>
-                    <div class="form-group mb-3">
+                    <div class="form-group">
                         <input type="password" name="confirmpassword" placeholder="Confirm Password" class="form-control">
                     </div>
-                    <div class="d-grid">
-                        <button type="submit" class="btn btn-dark">Submit</button>
+                    <div class="form-group">
+                        <button type="submit" class="btn">Register</button>
                     </div>
                 </form>
+                <div class="social-login">
+                    <a href="http://gmail.com" class="btn-social google">G Google</a>
+                    <a href="http://facebook.com" class="btn-social facebook">f Facebook</a>
+                </div>
             </div>
         </div>
     </div>
 </body>
+
 </html>
